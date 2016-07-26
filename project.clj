6b954ae8e9cd172e49df0692e6633406bf9718cb
Anthony Galea <anthony.galea@gmail.com>
2016-07-26 14:45:56 +0200

    add spec

--- project.clj
+++ project.clj
@@ -1,3 +1,5 @@
 (defproject garage-simulation "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.8.0"]]
-  :profiles {:dev {:dependencies [[midje "1.8.3"]]}})
+  :dependencies [[org.clojure/clojure "1.9.0-alpha10"]]
+  :profiles {:dev {:dependencies [[midje "1.8.3"]
+                                  [org.clojure/test.check "0.9.0"]
+                                  [com.gfredericks/test.chuck "0.2.6"]]}})
--- src/garage_simulation/core.clj
+++ src/garage_simulation/core.clj
@@ -1,22 +1,47 @@
-(ns garage-simulation.core)
+(ns garage-simulation.core
+  (:require [clojure.spec :as s]
+            [clojure.spec.gen :as g]
+            [clojure.spec.test :as t]
+            [com.gfredericks.test.chuck.generators :as cg]))
 
-(def garage-levels {0 15
-                    1 10})
+(s/def ::number-of-parking-spaces (s/and int? pos?))
+
+(s/def ::parking-spaces (s/coll-of ::number-of-parking-spaces :kind vector?))
+
+(def parking-spaces [5 2])
 
 (def empty-parking-spaces (ref #{}))
 
 (def vehicles (ref {}))
 
-(defn initialize-garage
+(s/fdef initialize-garage!
+        :args (s/cat :parking-spaces ::parking-spaces))
+
+(defn initialize-garage!
   "Sets the initial state of the garage."
-  [levels]
+  [parking-spaces]
   (dosync
    (ref-set vehicles {})
    (ref-set empty-parking-spaces
             (into #{}
-                  (mapcat (fn [level]
-                            (map #(vector (key level) %) (range (val level))))
-                          levels)))))
+                  (apply concat
+                         (map-indexed (fn [level number-of-parking-spaces]
+                                        (map #(vector level %)
+                                             (range number-of-parking-spaces)))
+                                      parking-spaces))))))
+
+(def licence-plate-regex #"[A-Z]{4}\d{3}")
+
+(s/def ::licence-plate
+  (s/with-gen
+    (s/and string? #(re-matches licence-plate-regex %))
+    #(cg/string-from-regex licence-plate-regex)))
+
+(s/def ::parking-space (s/coll-of (s/and int? (s/or :zero zero? :positive pos?)) :kind vector? :count 2))
+
+(s/fdef locate-vehicle
+        :args (s/cat :licence-plate ::licence-plate)
+        :ret (s/or ::parking-space nil?))
 
 (defn locate-vehicle
   "Given a licence plate, returns the location of a vehicle as a vector with the
@@ -24,12 +49,22 @@
   [licence-plate]
   (@vehicles licence-plate))
 
+(s/fdef number-of-free-parking-spaces
+        :ret (s/and int? (s/or :zero zero? :positive pos?)))
+
 (defn number-of-free-parking-spaces
   "Returns the current number of free parking spaces in the garage."
   []
-   (count @empty-parking-spaces))
+  (count @empty-parking-spaces))
 
-(defn exit-garage
+(s/def ::vehicles (s/nilable (s/map-of ::licence-plate ::parking-space)))
+
+(s/fdef exit-garage!
+        :args (s/cat :licence-plate ::licence-plate)
+        :ret ::vehicles
+        :fn #(not (contains? (:ret %) (-> % :args :licence-plate))))
+
+(defn exit-garage!
   "Simulates a vehicle exiting the garage. Returns the state of the garage if
    such a vehicle exists in the garage, nil otherwise."
   [licence-plate]
@@ -39,7 +74,13 @@
        (alter empty-parking-spaces conj vehicle-location)
        (alter vehicles dissoc licence-plate)))))
 
-(defn enter-garage
+(s/fdef enter-garage!
+        :args (s/cat :licence-plate ::licence-plate)
+        :ret ::vehicles
+        :fn #(or (nil? (:ret %))
+                 contains? (:ret %) (-> % :args :licence-plate)))
+
+(defn enter-garage!
   "Simulates a vehicle entering the garage. Return the state of the garage if
    there is still free space, nil if no empty parking space exists."
   [licence-plate]
